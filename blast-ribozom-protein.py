b4b205066bfadeeebc153c166fe3ade28f44a618
zivse <zivseker88@gmail.com>
2022-06-04 17:56:09 +0300

    change to csv and try work with uniprot

--- blast-ribozom-protein.py
+++ blast-ribozom-protein.py
@@ -1,16 +1,39 @@
 import ssl  # monkey patch for BioPython 1.68 & 1.69
+import csv
+import requests as r
+from Bio import SeqIO
+from io import StringIO
+from bs4 import BeautifulSoup
 ssl._create_default_https_context = ssl._create_unverified_context
 from Bio.Blast import NCBIWWW
 
-# sequence_data = open("sequence.fasta").read()
-# print(sequence_data)
-protein = "MRPS28"
+
 with open('protein-ribozom2.txt') as f:
     proteins = f.readlines()
 for protein in proteins:
-    result_handle = NCBIWWW.qblast("blastp", "nt", protein)
-    with open('results.xml', 'a') as save_file:
+    baseUrl = "http://www.uniprot.org/uniprot/"
+    currentUrl = baseUrl + 'P04637' + ".fasta"
+    response = r.post(currentUrl)
+    cData = ''.join(response.text)
+    Seq = StringIO(cData)
+    pSeq = list(SeqIO.parse(Seq, 'fasta'))[0].seq
+    result_handle = NCBIWWW.qblast("blastp", "nt", pSeq)
+    with open('names.csv', 'w', newline='') as csvfile:
+        fieldnames = ['query_id', 'protein_name', 'query_len']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         blast_results = result_handle.read()
-        save_file.write(blast_results)
-    print(f"done with {protein}")
+        Bs_data = BeautifulSoup(blast_results, "xml")
+        query_id = Bs_data.find_all('BlastOutput_query-ID')[0].contents[0]
+        protein_name = Bs_data.find_all('BlastOutput_query-def')[0].contents[0]
+        query_len = Bs_data.find_all('BlastOutput_query-len')[0].contents[0]
+        writer.writeheader()
+        writer.writerow({'query_id': query_id, 'protein_name':protein_name, 'query_len': query_len})
+
+
+
+
+
+
+
+
 

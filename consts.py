b07981699cd263a505b474decb8ebea0bc95da4a
zivse <zivseker88@gmail.com>
2023-07-23 11:43:56 +0300

    change db

--- consts.py
+++ consts.py
@@ -4,5 +4,6 @@ SEQ_FILES_NAME = 'seq-files'
 CSV_FILES_NAME = 'csv-files'
 MUSCLE_FILES_NAME = 'muscle-files'
 MUSCLE_EXE = r"/gpfs0/biores/users/mishmarlab/my_conda_envs/noam_env_min/bin/muscle"
-DATABASE = 'refseq_protein'
+DATABASE = 'nr'
 NR = 'nr'
+REF_SEQ = 'refseq_protein'
